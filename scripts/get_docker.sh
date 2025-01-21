fbfbd1106a4fa46345e656af08270529aa7ddd64
Benned-H <benned1@gmail.com>
2025-01-21 11:35:44 -0500

    WIP: Edit Docker image retrieval script

--- scripts/get_docker.sh
+++ scripts/get_docker.sh
@@ -20,8 +20,17 @@ if [[ $machine == "Linux" ]]; then
     nvidiaGPU="$(expr "${lshwOut}" == *NVIDIA*)"
     if [[ $nvidiaGPU ]]; then
         smiOut="$(nvidia-smi)"
-        echo "Exit status of nvidia-smi was $?"
+        smiStatus="$?"
+        if [[ $smiStatus -eq 0 ]]; then
+            echo "Exit status of nvidia-smi was $smiStatus; OK"
+        else
+            echo "Error: Exit status of nvidia-smi was $smiStatus"
+            echo "$smiOut"
+            exit 1
+        fi
     fi
 else
     nvidiaGPU=false # macOS don't support NVIDIA GPUs
 fi
+
+# Select Docker service based on identified environment details
