f89dced620bb27b3dce7efd2801b5a40349bd5b8
Benned-H <benned1@gmail.com>
2025-01-20 23:06:34 -0500

    Continue bash script for obtaining Docker image

--- scripts/get_docker.sh
+++ scripts/get_docker.sh
@@ -5,9 +5,23 @@ unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)     machine=Linux;;
     Darwin*)    machine=Mac;;
-    CYGWIN*)    machine=Cygwin;;
-    MINGW*)     machine=MinGw;;
-    MSYS_NT*)   machine=MSys;;
-    *)          machine="UNKNOWN:${unameOut}"
+    *)          machine="Unsupported:${unameOut}"
 esac
 echo "Detected local environment as ${machine}."
+
+if [[ $machine == Unsupported:* ]]; then
+    echo "Your environment is not supported. Please use a Linux or macOS machine."
+    exit 1
+fi
+
+# Check for NVIDIA GPU on any Linux host machine
+if [[ $machine == "Linux" ]]; then
+    lshwOut="$(lshw -C display)"
+    nvidiaGPU="$(expr "${lshwOut}" == *NVIDIA*)"
+    if [[ $nvidiaGPU ]]; then
+        smiOut="$(nvidia-smi)"
+        echo "Exit status of nvidia-smi was $?"
+    fi
+else
+    nvidiaGPU=false # macOS don't support NVIDIA GPUs
+fi
