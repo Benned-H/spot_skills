fc72d58b18454bfb993e8547381c0b280254d314
Benned Hedegaard <benned1@gmail.com>
2025-01-31 17:50:45 -0500

    Improve tmux setup script to handle tmux install/config

--- docker/tmux-launch.sh
+++ docker/tmux-launch.sh
@@ -1,47 +1,48 @@
 #!/bin/bash
 
-# Begin by verifying proper environment setup
-check_directory () {
-	local dir_path="$1"
-	
-	if [ ! -d "$dir_path" ]; then
-		echo "Error: Expected to find directory $dir_path"
-		exit 1
-	fi
-}
-
-check_repo() {
-	local dir_path="$1"
-	local expected_repo_url="$2"
-
-	check_directory "$dir_path"
-	
-	local working_dir=$(pwd)
-	cd "$dir_path" || exit 1
-	
-	local found_repo_url=$(git config --get remote.origin.url)
-	if [ "$expected_repo_url" != "$found_repo_url" ]; then
-		echo "Error: Expected directory ${dir_path} to have remote URL '${expected_repo_url}' but instead found '${found_repo_url}'"
-		exit 1
-	else
-		echo "Confirmed that directory ${dir_path} has remote URL '${found_repo_url}'"
-	fi
-
-	cd "$working_dir"
-}
+# Move to the directory of this script
+cd $(dirname "$0") || exit 1
+source ros_docker/scripts/filesystem.sh
 
 spot_skills_path="$HOME/Documents/spot_skills"
 spot_skills_remote="git@github.com:Benned-H/spot_skills.git"
 pose_path="$HOME/Documents/pose"
 pose_remote="git@github.com:SpotIncarnated/pose.git"
 
-check_repo "$spot_skills_path" "$spot_skills_remote"
-check_repo "$pose_path" "$pose_remote"
+check_repo "$HOME/Documents/spot_skills" "git@github.com:Benned-H/spot_skills.git"
+check_repo "$HOME/Documents/pose" "git@github.com:SpotIncarnated/pose.git"
+
+# Verify that tmux is installed (script can't install it itself)
+source ros_docker/scripts/commandline.sh
+check_command "tmux" "sudo apt install tmux"
+
+# If the user doesn't have a tmux config, install a baseline ~/.tmux.conf
+tmux_conf="$HOME/.tmux.conf"
+tmux_raw_gist_url="https://gist.githubusercontent.com/Benned-H/6b6836e02d4b9bd2974ac0b8572800fe/raw/1053e3c72f1312db07f11a38a7546793c4f6033e/.tmux.conf"
+tpm_path="$HOME/.tmux/plugins/tpm"
+
+if [ ! -f "$tmux_conf" ]; then
+	wget -O "$tmux_conf" "$tmux_raw_gist_url"
+else
+	echo "${tmux_conf} already exists; leaving as-is..."
+fi
+check_file "$tmux_conf"
+
+# Check for tmux plugin manager (tpm), install if missing
+if [ ! -d "$tpm_path" ]; then
+	git clone https://github.com/tmux-plugins/tpm "$tpm_path"
+fi
+tmux source "$tmux_conf"
+"${tpm_path}/bin/install_plugins"
+
+# Ask for user input once all environment verification is complete
+echo "Environment verification is complete. Press any key to continue..."
+read -n1
 
 # Create the tmux session
-
 session="pose-estimate"
-tmux new-session -d -s $session
+tmux kill-session -t "$session"
+tmux new-session -d -s "$session"
 
 # Window 1: Docker containers
 w1="docker"
@@ -50,13 +51,13 @@ tmux send-keys -t "${session}:${w1}" "xhost +local:docker" C-m
 tmux split-window -h
 
 # Window 1.1 (left): Spot Skills
-tmux send-keys -t {left} "cd $spot_skills_path" C-m "bash docker/launch.sh" C-m \
-	"source devel/setup.bash" C-m "clear" C-m "roslaunch spot_skills pose_estimation_demo.launch"
+tmux send-keys -t {left} "cd $spot_skills_path" C-m "bash docker/launch.sh" C-m "source devel/setup.bash" C-m \
+	"clear && echo -e 'Launch the Spot bringup nodes using the command:\n    roslaunch spot_skills pose_estimation_demo.launch'" C-m
 
 # Window 1.2 (right): Pose
 tmux send-keys -t {right} "cd $pose_path" C-m "sh docker/run_container_ros1.sh" C-m \
-	"cd /docker/pose/pose_ws" C-m "catkin_make" C-m "cd /docker/pose" C-m "clear" C-m \
-	"sh /docker/pose/run_service.sh"
+	"cd /docker/pose/pose_ws" C-m "catkin_make" C-m "cd /docker/pose" C-m \
+	"clear && echo -e 'Launch the pose estimation server using the command:\n    sh /docker/pose/run_service.sh'" C-m
 
 # Window 2: Git (again, spot_skills and pose)
 w2="git"
