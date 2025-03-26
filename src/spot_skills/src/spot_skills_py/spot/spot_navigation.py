7722c232dcd95d03bc844bf218a349f6faf19401
Benned-H <benned1@gmail.com>
2025-03-25 23:38:07 -0400

    Finish integrating navigation changes into Spot Wrapper

--- src/spot_skills/src/spot_skills_py/spot/spot_navigation.py
+++ src/spot_skills/src/spot_skills_py/spot/spot_navigation.py
@@ -2,10 +2,14 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 import rospy
 from actionlib import GoalStatus, SimpleActionClient
-from geometry_msgs.msg import PoseStamped
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from transform_utils.kinematics_ros import pose_from_msg, pose_to_stamped_msg
+from transform_utils.math.distances import absolute_yaw_error_rad, euclidean_distance_m
+from transform_utils.transform_manager import TransformManager
 
 from spot_skills.srv import (
     NavigateToLandmark,
@@ -15,13 +19,23 @@ from spot_skills.srv import (
     NavigateToPoseRequest,
     NavigateToPoseResponse,
 )
+from spot_skills_py.spot.spot_manager import SpotManager
+
+if TYPE_CHECKING:
+    from geometry_msgs.msg import PoseStamped
+    from transform_utils.kinematics import Pose2D, Pose3D
 
 
 class SpotNavigationServer:
     """A wrapper for ROS services controlling Spot's navigation."""
 
-    def __init__(self) -> None:
-        """Initialize the ROS services provided by this class."""
+    def __init__(self, manager: SpotManager) -> None:
+        """Initialize the ROS services provided by this class.
+
+        :param manager: SpotManager object used to control Spot through the Spot SDK
+        """
+        self._manager = manager
+
         self._nav_to_pose_srv = rospy.Service(
             "/spot/navigate_to_pose",
             NavigateToPose,
@@ -34,7 +48,33 @@ class SpotNavigationServer:
             self.handle_landmark,
         )
 
+        # Wait for the move_base action server to become available
         self._move_base_client = SimpleActionClient("move_base", MoveBaseAction)
+        self._move_base_client.wait_for_server(timeout=rospy.Duration.from_sec(60.0))
+
+        self._landmarks: dict[str, Pose3D] = {}  # TODO: Fill in from YAML
+
+        # Load thresholds for when Spot is considered "close to a goal" from ROS params
+        self.close_to_goal_m = rospy.get_param("/spot_navigation/close_to_goal_m")
+        self.close_to_goal_rad = rospy.get_param("/spot_navigation/close_to_goal_rad")
+        self.timeout_s = rospy.get_param("/spot_navigation/timeout_s")
+
+    def check_close_to_goal(self, target_pose_2d: Pose2D) -> bool:
+        """Check whether Spot is currently "close" to a goal pose, using the stored thresholds.
+
+        :param target_pose_2d: Target base pose for Spot
+        :return: True if Spot is "close" to the given target pose, else False
+        """
+        now = rospy.Time.now()
+        curr_pose = TransformManager.lookup_transform("body", target_pose_2d.ref_frame, now)
+        if curr_pose is None:
+            rospy.logfatal(f"Could not look up body pose in frame '{target_pose_2d.ref_frame}'.")
+            return False
+
+        distance_2d_m = euclidean_distance_m(target_pose_2d, curr_pose.to_2d())
+        abs_yaw_error_rad = absolute_yaw_error_rad(target_pose_2d, curr_pose)
+
+        return distance_2d_m < self.close_to_goal_m and abs_yaw_error_rad < self.close_to_goal_rad
 
     def handle_pose(self, request: NavigateToPoseRequest) -> NavigateToPoseResponse:
         """Handle a ROS service request for Spot to navigate to a given pose.
@@ -51,18 +91,29 @@ class SpotNavigationServer:
         :param request: Request specifying a landmark to navigate to
         :return: Response specifying whether the navigation succeeded
         """
-        # TODO: Lookup pose imported from YAML
-        pose_msg = PoseStamped()
-        success, message = self.navigate_to_pose(pose_msg)
+        if request.landmark_name not in self._landmarks:
+            message = f"Cannot navigate to unknown landmark '{request.landmark_name}'."
+            return NavigateToLandmarkResponse(success=False, message=message)
 
-    def navigate_to_pose(self, pose_msg: PoseStamped, timeout_s: float) -> tuple[bool, str]:
+        target_pose = self._landmarks[request.landmark_name]
+        pose_stamped_msg = pose_to_stamped_msg(target_pose)
+        success, message = self.navigate_to_pose(pose_stamped_msg)
+        return NavigateToLandmarkResponse(success, message)
+
+    def navigate_to_pose(self, pose_msg: PoseStamped) -> tuple[bool, str]:
         """Control Spot to navigate to the given pose (treated as if it were 2D).
 
         :param pose_msg: ROS message representing a target body pose for Spot
-        :param timeout_s: Duration (seconds) after which the action will time out
-        :return: Boolean indicating success (True) or failure (False) and a string message
+        :return: Boolean success indicator (True if action succeeds) and message explaining why
         """
-        duration_timeout = rospy.Duration.from_sec(timeout_s)
+        has_control = self._manager.check_control()  # Only take control of Spot once necessary
+        if not has_control:
+            has_control = self._manager.check_control()
+
+        if not has_control:
+            return False, "Could not obtain control of Spot using the SpotManager."
+
+        duration_timeout = rospy.Duration.from_sec(self.timeout_s)
 
         action_goal = MoveBaseGoal(target_pose=pose_msg)
         self._move_base_client.send_goal_and_wait(action_goal, execute_timeout=duration_timeout)
@@ -70,13 +121,18 @@ class SpotNavigationServer:
         result_state = self._move_base_client.get_state()
         result_text = self._move_base_client.get_goal_status_text()
 
+        target_pose_2d = pose_from_msg(pose_msg).to_2d()
+
+        # If move_base didn't bring Spot sufficiently close to the goal, give up
         if result_state != GoalStatus.SUCCEEDED:
             rospy.loginfo(f"MoveBase failed with state {result_state} and text '{result_text}'.")
 
-            # TODO: If sufficiently close to goal, use SDK to try to reach it
-            # Otherwise, exit with failure
+            if not self.check_close_to_goal(target_pose_2d):
+                return (False, f"Move base failed with message: {result_text}")
 
-        # Use the Spot SDK for fine-grained control
-        # TODO: Where is `go_to_pose2` being used? Why aren't we using that directly?
+        rospy.log_info("Spot has successfully moved 'close' to the goal using move_base.")
 
-        # TODO: Continue with integration
+        success = self._manager.navigate_to_base_pose(target_pose_2d, self.timeout_s)
+        message = "Navigation was successful." if success else "Navigation failed."
+
+        return success, message
