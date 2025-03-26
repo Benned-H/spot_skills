8c1d98cde78c8386f19cd7c1f99387b13fc51abf
Benned-H <benned1@gmail.com>
2025-03-26 17:28:43 -0400

    Add package dependencies to the pip-install script

--- docker/pip_install.sh
+++ docker/pip_install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+# Install dependencies of the project's Python packages
+pip install -r /docker/spot_skills/src/spot_skills/requirements.txt
+
 # Install the project's Python packages on the local machine
 # Reference: https://stackoverflow.com/a/35064498/10278033
-
 pip install -e /docker/spot_skills/src/spot_skills
